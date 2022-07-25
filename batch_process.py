0b8cc4819c3c5eecab02028756e3ec34c4dce762
SequelSaturn <108498033+SequelSaturn@users.noreply.github.com>
2022-07-24 21:22:58 -0400

    Update batch_process.py

--- batch_process.py
+++ batch_process.py
@@ -1,7 +1,6 @@
 import os
 import time
 
-for jj in range(1,105):
+for jj in range(2,105):
     str0 = "python projector.py --outdir=out/projector" + str(jj) + " --target=out/ganstorage/final_imag00"+ str(jj) + ".jpg --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada/pretrained/ffhq.pkl"
-    os.system(str0)
-    time.sleep(15*60) 
+    os.system(str0) 
