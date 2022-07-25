ae06b41a7c2c9ad14148f49f95c481c1ef8df8b3
SequelSaturn <108498033+SequelSaturn@users.noreply.github.com>
2022-07-24 20:31:55 -0400

    Add files via upload

---
+++ batch_process.py
@@ -0,0 +1,7 @@
+import os
+import time
+
+for jj in range(1,105):
+    str0 = "python projector.py --outdir=out/projector" + str(jj) + " --target=out/ganstorage/final_imag00"+ str(jj) + ".jpg --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada/pretrained/ffhq.pkl"
+    os.system(str1)
+    time.sleep(15*60) 
